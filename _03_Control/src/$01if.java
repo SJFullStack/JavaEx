c75e5e923f32ff0c2ad6762b41035fba47c1005e
SJFullstack <bbodaewall@gmail.com>
2025-05-12 15:25:36 +0900

    if ~ else문 실습

--- _03_Control/src/$01if.java
+++ _03_Control/src/$01if.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+public class $01if {
+public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("점수 입력 >> ");
@@ -9,11 +9,9 @@ public class App {
 
         if(score >= 90)
             System.err.println("A학점");
-
-        // if(score >= 90){
-        //     System.err.println("A학점");
-        // }            
-
+        else
+            System.out.println("A학점이 아닙니다.");
+        
         sc.close();
     }
 }
