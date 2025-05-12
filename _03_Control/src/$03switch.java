80d23aa2a0984acde968b4579443cc4b3180609e
SJFullstack <bbodaewall@gmail.com>
2025-05-12 16:27:07 +0900

    제어문 실습 완료료

--- _03_Control/src/$03switch.java
+++ _03_Control/src/$03switch.java
@@ -7,28 +7,31 @@ public class $03switch {
         System.out.println("3. 탕수육");
         System.out.println("4. 유산슬");
         System.out.println("5. 유린기");
-        System.out.println("선택 >> ");
+        System.out.print("선택 >> ");
 
         Scanner sc = new Scanner(System.in);
         int sel = sc.nextInt();
-        switch(sel);
-            case 1.
+        switch(sel){
+            case 1:
                 System.out.println("짜장면이 나왔습니다.");
                 break;
-            case 2.
+            case 2:
                 System.out.println("짬뽕이 나왔습니다.");
                 break;
-            case 3.
+            case 3:
                 System.out.println("탕수육이 나왔습니다.");
                 break;
-            case 4.
+            case 4:
                 System.out.println("유산슬이 나왔습니다.");
                 break;
-            case 5.
+            case 5:
                 System.out.println("유린기가 나왔습니다.");
                 break;
-            default:            //else의 의미
+            default:        // else의 의미
                 System.out.println("해당 메뉴 번호가 없습니다.");
-                break;   
+                break;
+        }
+
+        sc.close();
     }
 }
